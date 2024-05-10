5f4f76b1a600352bf75ff3a8145f8ea7106471e7
spdini <sipo.dini@gmail.com>
2024-05-10 10:58:19 +0700

    Update perfume_recommender.py

--- perfume_recommender.py
+++ perfume_recommender.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-import request
+import requests
 
 # Load the perfume data from GitHub
 url = "https://raw.githubusercontent.com/spdini/perfume_recommender/main/Perfume%20Data.csv"
